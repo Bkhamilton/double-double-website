5f707a89e39e5b98a3d39f67a43b7bad309d6d54
Bkhamilton <bkhamilton310@gmail.com>
2025-03-26 23:29:28 -0400

    updated footer styling to use Tailwind CSS

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,30 +1,44 @@
 import * as React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import './Footer.css';
-import logo from '../../images/Double Double Logo.jpg';
 
 export default function Footer() {
     return (
-        <div className="footer">
-          <ul className="footer-links">
-            <li className='footer-item'>
-              <Link href="/" className="footer-link"><span className='footer-link-text'>Home</span></Link>
-            </li>
-            <li className='footer-item'>
-              <Link href="/draft-guide" className="footer-link"><span className='footer-link-text'>Draft Guide</span></Link>
-            </li>
-            <li className='footer-item'>
-              <Link href="/blog" className="footer-link"><span className='footer-link-text'>Blog</span></Link>
-            </li>
-            <li className='footer-item'>
-              <Link href="/contact-us" className="footer-link"><span className='footer-link-text'>Contact Us</span></Link>
-            </li>
-          </ul>
-          <div className="footer-info">
-            <Image src={'/logo.jpg'} alt="logo" className="footer-logo" width={200} height={200}/>
-            <p className="footer-message"><span className='footer-design-text'>Designed by Ben Hamilton</span></p>
-          </div>
+        <div className="bg-gray-50 px-[12%] py-[3%] flex justify-between items-center md:px-[4%] sm:px-[2%] sm:py-[4%]">
+            <ul className="block list-none">
+                <li className="my-[10px] whitespace-nowrap sm:my-[5px] sm:py-[1%]">
+                    <Link href="/" className="mr-[15px] text-gray-800 no-underline hover:text-blue-600 transition-colors duration-300">
+                        <span className="font-roboto text-[26px] font-bold sm:text-[20px]">Home</span>
+                    </Link>
+                </li>
+                <li className="my-[10px] whitespace-nowrap sm:my-[5px] sm:py-[1%]">
+                    <Link href="/draft-guide" className="mr-[15px] text-gray-800 no-underline hover:text-blue-600 transition-colors duration-300">
+                        <span className="font-roboto text-[26px] font-bold sm:text-[20px]">Draft Guide</span>
+                    </Link>
+                </li>
+                <li className="my-[10px] whitespace-nowrap sm:my-[5px] sm:py-[1%]">
+                    <Link href="/blog" className="mr-[15px] text-gray-800 no-underline hover:text-blue-600 transition-colors duration-300">
+                        <span className="font-roboto text-[26px] font-bold sm:text-[20px]">Blog</span>
+                    </Link>
+                </li>
+                <li className="my-[10px] whitespace-nowrap sm:my-[5px] sm:py-[1%]">
+                    <Link href="/contact-us" className="mr-[15px] text-gray-800 no-underline hover:text-blue-600 transition-colors duration-300">
+                        <span className="font-roboto text-[26px] font-bold sm:text-[20px]">Contact Us</span>
+                    </Link>
+                </li>
+            </ul>
+            <div className="flex flex-col items-center">
+                <Image 
+                    src={'/logo.jpg'} 
+                    alt="logo" 
+                    className="h-[200px] w-auto sm:h-[150px] sm:ml-[35px]" 
+                    width={200} 
+                    height={200}
+                />
+                <p className="ml-[10px]">
+                    <span className="font-roboto text-[16px]">Designed by Ben Hamilton</span>
+                </p>
+            </div>
         </div>
-      );
+    );
 };
